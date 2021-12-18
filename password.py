478f377bbb7dba31909a961093a7576ad2fd2c56
Leandro Heck <leandro.h@sou.unijui.edu.br>
2021-12-18 18:44:19 -0300

    Numero de senhas
    add funcionalidade para gerar numero x de senhas informada pelo usuario

--- password.py
+++ password.py
@@ -19,17 +19,26 @@ def pass_gen():
     pass_list.extend(string_punctuation)
 
     # embaralha os caracteres da lista(reorganiza a ordem dos itens da lista)
-    random.shuffle(pass_list)
+    #random.shuffle(pass_list)
 
     # obter o comprimento da senha a partir da entrada do utilizador
-    pass_length = int(input("Insira o comprimento (tamanho) da senha : "))
+    pass_length = int(input("Insira o comprimento (tamanho) da senha: "))
+    number_password = int(input("Insira a quantidade de senhas a serem geradas: "))
 
-    # converter a lista de palavras-passe em cadeia do índice
-    # 0 para o comprimento da palavra-passe introduzida pelo utilizador
-    password_result = "".join(pass_list[0:pass_length])
+    num = 0
+    #password_list = []
+    while number_password != num:
+        # converter a lista de palavras-passe em cadeia do índice
+        # 0 para o comprimento da palavra-passe introduzida pelo utilizador
+        random.shuffle(pass_list)
+        password_result = "".join(pass_list[0:pass_length])
+        #password_list.append(password_result)
 
-    # imprimir palavra-passe gerada
-    print("A senha gerada é : ", password_result)
+        number_password -= 1
+
+        # imprimir palavra-passe gerada
+        print("A senha gerada é : ", password_result)
+
 
 
 pass_gen()
